6145fd5321b27126026dade731ebeeba1b5a2c3d
prithvirajbl280 <prithvirajbl14@gmail.com>
2025-11-24 21:13:44 +0530

    Update API baseURL to remove '/api' suffix

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api"
-  baseURL: "https://backend-production-fdc7.up.railway.app/api"
+  baseURL: "https://backend-production-fdc7.up.railway.app"
 
 });
 
