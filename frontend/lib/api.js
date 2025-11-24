b6e8b133280db253032e037db965417f5ce51931
prithvirajbl280 <prithvirajbl14@gmail.com>
2025-11-24 17:04:31 +0530

    Update api.js

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://backend-production-3992e.up.railway.app/api"
+  // baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api"
+  baseURL: "https://backend-production-3992e.up.railway.app"
+
 });
 
 API.interceptors.request.use((config) => {
