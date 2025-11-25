e78cf0382d65a47936e3175ad43095573841fb85
prithvirajbl280 <prithvirajbl14@gmail.com>
2025-11-25 09:57:42 +0530

    Create PasswordResetTokenRepository.kt

---
+++ backend/src/main/kotlin/com/ticketing/repository/PasswordResetTokenRepository.kt
@@ -0,0 +1,9 @@
+package com.ticketing.repository
+
+import com.ticketing.model.PasswordResetToken
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Optional
+
+interface PasswordResetTokenRepository : JpaRepository<PasswordResetToken, Long> {
+    fun findByToken(token: String): Optional<PasswordResetToken>
+}
