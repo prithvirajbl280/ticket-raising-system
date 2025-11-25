b7ec5d4912a1a957ba1f699f6b090ed4792c71d6
prithvirajbl280 <prithvirajbl14@gmail.com>
2025-11-25 10:03:18 +0530

    Update build.gradle.kts

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-mail")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
